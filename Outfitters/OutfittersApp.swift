ab4485d1a1619a403591816e24c2901f1c45277e
Vincent <vbn3@illinois.edu>
2021-10-30 00:25:58 -0500

    Initial Commit

---
+++ Outfitters/OutfittersApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfittersApp.swift
+//  Outfitters
+//
+//  Created by Vincent Nguyen on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct OutfittersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
